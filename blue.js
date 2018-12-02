d7b51288744aa263bdcd4b495de8640442869c9b
Daniel Watt <daniel@Daniels-MacBook-Air.local>
2018-12-02 19:00:17 +1300

    fixed bug where blue would fall through the platorm sometimes

--- blue.js
+++ blue.js
@@ -14,11 +14,11 @@ var gravity = 0.5;
 var friction = 0.80;
 var ground_level = 395;
 
-var blue_img = new Image();
+var blue_img = new Image(64, 64);
 blue_img.src = "./images/blue.png";
 
 const background_img = new Image();
-background_img.src = "./images/textures/cave_background.png";
+background_img.src = "./images/background.png";
 
 
 
@@ -170,6 +170,8 @@ function move_player(object_hitboxes) {
     blue_collision.x0 += x_velocity;
     blue_collision.y0 += y_velocity;
     
+    console.log(blue.width);
+    
     blue.y += y_velocity;
     blue.x += x_velocity;
     
@@ -188,6 +190,9 @@ function move_player(object_hitboxes) {
 
             } else {
                 y_direction_factor = blue_collision.y0 < box_collision.y0 ? -0.5 : 0.5;
+                console.log("collision");
+                console.log(blue_collision.y0);
+                console.log(box_collision.y0);
 
                 while (is_collision(blue_collision, box_collision)) {
                     blue.y += y_direction_factor;
@@ -198,6 +203,7 @@ function move_player(object_hitboxes) {
                     y_velocity = 0;
                     
                 } else { // only on platform when you're on top, touching from the bottom doesn't count
+                    console.log("on platform");
                     blue.is_on_platform = true;
                     blue.is_in_air = false;
                     blue.is_jumping = false;
@@ -205,7 +211,7 @@ function move_player(object_hitboxes) {
                 }
             }
         } else {
-            if (is_just_above(blue_collision, object_hitboxes, 0.5)) {//if blue is on the platform
+            if (is_just_above(blue_collision, object_hitboxes, 1)) {//if blue is on the platform
                 blue.is_on_platform = true;
 
             } else if (blue.is_on_platform) { //if blue was on a platform before, but is not on the platform now
